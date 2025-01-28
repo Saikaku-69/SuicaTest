c24926be742af2b1d0a10a495443a9786fa3975b
Saikaku-69 <grnmnsta@gmail.com>
2025-01-28 10:50:17 +0900

    Add Initial Files

---
+++ SuicaTest/SuicaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuicaTestApp.swift
+//  SuicaTest
+//
+//  Created by cmStudent on 2025/01/28.
+//
+
+import SwiftUI
+
+@main
+struct SuicaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
